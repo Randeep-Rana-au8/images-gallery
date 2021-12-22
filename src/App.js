fb4f9867fa05295a6bde074a94cf90960babff6e
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-12-22 16:19:06 +0530

    Added api for search and search functionlity is working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./App.css";
-import LoadImages from "./components/api";
+import LoadImages, { SearchImages } from "./components/api";
 import Image from "./components/Image";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [searchQ, setSearchQ] = useState();
   const Collection = LoadImages();
+  const SearchCollection = SearchImages(searchQ, setSearchQ);
+
+  const search = (e) => {
+    e.preventDefault();
+    setSearchQ(query);
+  };
 
   return (
     <div className="App">
-      {Collection.map((data, index) => (
-        <Image src={data.urls.thumb} alt={data.alt_description} key={index} />
-      ))}
+      <form onSubmit={search}>
+        <input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      <div className="container">
+        {SearchCollection.map((data, index) => (
+          <Image src={data.urls.thumb} alt={data.alt_description} key={index} />
+        ))}
+        {Collection.map((data, index) => (
+          <Image src={data.urls.thumb} alt={data.alt_description} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/api.js
+++ src/components/api.js
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const LoadImages = () => {
   const [state, setState] = useState([]);
-  axios
-    .get("https://api.unsplash.com/photos/?client_id=VP3vNPwuUesOW3z54cpzJJ1pUPqW4aUoPWYt5aGEI6Y")
-    .then((res) => setState(res.data))
-    .catch((e) => console.error(e));
+  useEffect(() => {
+    axios
+      .get("https://api.unsplash.com/photos/?client_id=VP3vNPwuUesOW3z54cpzJJ1pUPqW4aUoPWYt5aGEI6Y")
+      .then((res) => setState(res.data))
+      .catch((e) => console.error(e));
+  }, []);
 
   return state;
 };
 
 export default LoadImages;
+
+export const SearchImages = (query, setQuery) => {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    if (query) {
+      axios
+        .get(
+          "https://api.unsplash.com/search/photos?query=" +
+            query +
+            "&client_id=VP3vNPwuUesOW3z54cpzJJ1pUPqW4aUoPWYt5aGEI6Y"
+        )
+        .then((res) => {
+          setState(res.data.results);
+          setQuery("");
+        })
+        .catch((e) => console.error(e));
+    }
+  }, [query, setQuery]);
+
+  return state;
+};
